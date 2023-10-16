306a0cbd8e755de1d6ba6b7e50f4bf35f9fc64aa
carlitos-206 <carloscaceres041@gmail,com>
2023-10-15 19:42:15 -0700

    reviews is now responsive

--- src/components/_homePage/section_5/b_reviews/reviews.jsx
+++ src/components/_homePage/section_5/b_reviews/reviews.jsx
@@ -10,20 +10,26 @@ export default function Reviews() {
       <div className="reviews-content">
         <div className="reviews-content-block">
           <img className='reviews-user-icon' src={userIcon} alt="user-icon" />
-          <p className="reviews-user-fullName">Susie Heisenberg</p>
-          <p className="reviews-user-occupation">English Teacher</p>
-          <p className="reviews-user-text">“EasySpeak will change how language learners learn languages. I’ve seen many learners struggle with their pronunciation. I’m so happy there is a tool out there that will help with this problem. I can’t wait to try this app!”</p>
+          <div className="reviews-user-details">
+            <p className="reviews-user-fullName">Susie Heisenberg</p>
+            <p className="reviews-user-occupation">English Teacher</p>
+          </div>
+            <p className="reviews-user-text">“EasySpeak will change how language learners learn languages. I’ve seen many learners struggle with their pronunciation. I’m so happy there is a tool out there that will help with this problem. I can’t wait to try this app!”</p>
         </div>
         <div className="reviews-content-block">
           <img className='reviews-user-icon' src={userIcon} alt="user-icon"/>
+          <div className="reviews-user-details">
           <p className="reviews-user-fullName">Camille Dupont</p>
-          <p className="reviews-user-occupation">International Student</p>
+            <p className="reviews-user-occupation">International Student</p>
+          </div>
           <p className="reviews-user-text">“I’ve always been insecure with the way I speak. It’s hard enough trying to adapt to a new environment and with EasySpeak, this app has will help me so much in the way I speak.”</p>
         </div>
-        <div className="reviews-content-block">
+        <div className="reviews-content-block reviews-content-block-3">
           <img className='reviews-user-icon' src={userIcon} alt="user-icon"/>
-          <p className="reviews-user-fullName">Park Jae-hoon</p>
-          <p className="reviews-user-occupation">Business worker</p>
+          <div className="reviews-user-details">
+            <p className="reviews-user-fullName">Park Jae-hoon</p>
+            <p className="reviews-user-occupation">Business worker</p>
+          </div>
           <p className="reviews-user-text">“I am starting a new job in the United States, but my English isn’t very good. EasySpeak will help me speak more fluently so that I can perform better at my job and open more opportunities in my career.”</p>
         </div>
       </div>
