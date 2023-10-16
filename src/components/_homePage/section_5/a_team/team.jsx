bde23a5e6fd8b82ff89f947c1c10845c42d7d9bb
carlitos-206 <carloscaceres041@gmail,com>
2023-10-15 19:08:14 -0700

    team  update 2

--- src/components/_homePage/section_5/a_team/team.jsx
+++ src/components/_homePage/section_5/a_team/team.jsx
@@ -4,7 +4,7 @@ import "./team.css";
 export default function Team() {
   return (
     <div className="team-container">
-      <p className="team-intro">“We are proud to introduce our revolutionary language learning app that harnesses the latest in AI technology. Speaking with your own unique voice, our app will guide you towards mastering foreign languages and improving your pronunciation.”</p>
+      <p className="team-intro">“We are proud to introduce our <span className="team-highlight">revolutionary</span> language learning app that harnesses the latest in AI technology. Speaking with your own <span className="team-highlight">unique voice</span>, our app will guide you towards mastering foreign languages and improving your pronunciation.”</p>
       <p className="team-name">EasySpeak Team</p>
     </div>
   );
