ca9115113e801a8195a933fa15cb5b213d6b0e0d
carlitos-206 <carloscaceres041@gmail,com>
2023-10-15 22:10:29 -0700

    site is fully responsive

--- src/components/_aboutPage/section_2/section_2.jsx
+++ src/components/_aboutPage/section_2/section_2.jsx
@@ -19,18 +19,25 @@ export default function SectionTwo() {
             <p className='team-block-name'>Andrea Aguilera</p>
             <p className='team-block-title'>Co-Founder, Chief Executive Officer</p>
           </div>
-          <div className='team-block' id='team-block-ali'>
+          {/* <div className='team-block' id='team-block-ali'>
             <img className='team-member-profile' src={ali} alt="Andrea" />
             <p className='team-block-name'>Ali Khosro</p>
             <p className='team-block-title'>Co-Founder, Chief Technical Officer</p>
-          </div>
+          </div> */}
           <div className='team-block' id='team-block-aldrich'>
             <img className='team-member-profile' src={aldrich} alt="Andrea" />
             <p className='team-block-name'>Aldrich Lapid</p>
             <p className='team-block-title'>Co-Founder, Chief Design Officer</p>
           </div>
+          
+          <div className='team-block' id='team-block-carlos'>
+            <img className='team-member-profile' src={carlos} alt="Andrea" />
+            <p className='team-block-name'>Carlos Caceres</p>
+            <p className='team-block-title'>Software Engineer</p>
+          </div>
+
         </div>
-        <div className='about-section-2-content-block'>
+        {/* <div className='about-section-2-content-block'>
           <div className='team-block' id='team-block-carlos'>
             <img className='team-member-profile' src={carlos} alt="Andrea" />
             <p className='team-block-name'>Carlos Caceres</p>
@@ -46,7 +53,7 @@ export default function SectionTwo() {
             <p className='team-block-name_empty_'>_empty_</p>
             <p className='team-block-title_empty_'>Co-Founder, Chief Design Officer</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
